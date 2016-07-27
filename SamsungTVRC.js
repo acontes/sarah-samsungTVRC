9c107f7d3a85d118747c4ee7ba4c7d37dc065898
Arnaud Contes <arnaud.contes@gmail.com>
2016-07-27 21:34:16 +0200

    initial release

---
+++ SamsungTVRC.js
@@ -0,0 +1,31 @@
+﻿
+exports.action = function(data, callback, config, SARAH){
+	console.log(config.description)
+
+	var config=config.modules.SamsungTVRC
+	var iptv=config.IPtv
+	var modeltv=config.Modeltv
+	
+    var SamsungRemote = require('./lib/samsung-remote');
+
+    var remote = new SamsungRemote({
+       ip: iptv // required: IP address of your Samsung Smart TV 
+    });
+	
+	var command =data.command
+	var instructions=""
+	var instructiona=""
+	var instructionb=""
+		
+	console.log(iptv +'( Model:'+modeltv+ ' ) la commande '+ command)
+	
+	
+	remote.send(command, function callback(err) {
+    if (err) {
+        console.log( "impossible de faire l action");
+    } 
+});
+	
+   callback({ 'tts': ""});
+  
+}
