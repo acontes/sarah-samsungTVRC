0bb85917491b6d2fb873df538a99914f530b0026
acontes <arnaud.contes@gmai.com>
2016-07-28 11:40:39 +0200

    Add support for multiple commands

--- SamsungTVRC.js
+++ SamsungTVRC.js
@@ -5,27 +5,23 @@ exports.action = function(data, callback, config, SARAH){
 	var config=config.modules.SamsungTVRC
 	var iptv=config.IPtv
 	var modeltv=config.Modeltv
-	
+
     var SamsungRemote = require('./lib/samsung-remote');
 
     var remote = new SamsungRemote({
-       ip: iptv // required: IP address of your Samsung Smart TV 
+       ip: iptv // required: IP address of your Samsung Smart TV
     });
-	
-	var command =data.command
-	var instructions=""
-	var instructiona=""
-	var instructionb=""
-		
-	console.log(iptv +'( Model:'+modeltv+ ' ) la commande '+ command)
-	
-	
-	remote.send(command, function callback(err) {
-    if (err) {
-        console.log( "impossible de faire l action");
-    } 
-});
-	
+
+	var commands =data.command
+
+	commands.forEach(function(element, index, array) {
+		remote.send(element, function callback(err) {
+    		if (err) {
+        		console.log( "impossible de faire l'action " + element);
+    		}
+		});
+	});
+
    callback({ 'tts': ""});
-  
+
 }
